6784ae4545ed5841f3ba67dce6e2e3dd4728e228
Abdurashid <abdurashiddev@gmail.com>
2022-08-01 16:26:44 +0500

    students database conf done

--- database/migrations/2022_08_01_112313_create_students_table.php
+++ database/migrations/2022_08_01_112313_create_students_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,6 +14,9 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->string('full_name');
+            $table->string('address')->nullable();
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
