6b37a2efb78139d787d8629740b93ad95cd026f4
Abdurashid <abdurashiddev@gmail.com>
2022-08-04 09:47:29 +0500

    hint added to attendance show

--- resources/views/admin/attendances/show.blade.php
+++ resources/views/admin/attendances/show.blade.php
@@ -5,7 +5,8 @@
                 {{$group->name}} o'quvchilarining davomatlari
             </h2>
             <a href="{{route('attendance.edit', $group->id)}}"
-               class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 dark:focus:bg-blue-700">
+               class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 dark:focus:bg-blue-700"
+               title="Oxirgi davomatni tahrirlash">
                 <i class="fas fa-edit"></i>
             </a>
         </div>
@@ -14,6 +15,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-4">
+                @include('admin.attendances.hint')
                 <div class="overflow-x-auto relative shadow-md sm:rounded-lg">
                     <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
@@ -45,7 +47,6 @@
                         </tbody>
                     </table>
                 </div>
-
             </div>
         </div>
     </div>
