49215abefbe4b7499a28644d6ddd1df05b6a809a
Abdurashid <abdurashiddev@gmail.com>
2022-08-05 17:16:45 +0500

    exit link added to attendance create

--- resources/views/admin/attendances/create.blade.php
+++ resources/views/admin/attendances/create.blade.php
@@ -4,14 +4,21 @@
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                 {{$group->name}}
             </h2>
-            @if($students->count() > 0)
-                <button type="submit"
-                        form="attendance-form"
-                        class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 dark:focus:bg-blue-700">
-                    <i class="fas fa-edit"></i>
-                    Saqlash
-                </button>
-            @endif
+            <div class="flex gap-1">
+                <a href="{{route('attendance.index')}}"
+                   class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 dark:focus:bg-blue-700"
+                   title="Orqaga">
+                    <i class="fas fa-arrow-left"></i>
+                </a>
+                @if($students->count() > 0)
+                    <button type="submit"
+                            form="attendance-form"
+                            class="block text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 dark:focus:bg-green-700">
+                        <i class="fas fa-edit"></i>
+                        Saqlash
+                    </button>
+                @endif
+            </div>
         </div>
     </x-slot>
     <div class="py-12">
--- resources/views/admin/attendances/show.blade.php
+++ resources/views/admin/attendances/show.blade.php
@@ -7,7 +7,7 @@
             <div class="flex gap-1">
                 <a href="{{route('attendance.index')}}"
                    class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 dark:focus:bg-blue-700"
-                   title="Oxirgi davomatni tahrirlash">
+                   title="Orqaga">
                     <i class="fas fa-arrow-left"></i>
                 </a>
                 @if($group->students->count() > 0)
