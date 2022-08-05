19ee53006111dfca183227c7132d1b9ec2528562
Abdurashid <abdurashiddev@gmail.com>
2022-08-05 17:02:31 +0500

    exit link added to student create blade

--- resources/views/admin/groups/edit.blade.php
+++ resources/views/admin/groups/edit.blade.php
@@ -6,7 +6,7 @@
             </h2>
             <a
                 class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 dark:focus:bg-blue-700"
-                href="{{route('groups.create')}}">
+                href="{{route('groups.index')}}">
                 <i class="fas fa-arrow-left"></i> Orqaga
             </a>
         </div>
--- resources/views/admin/students/create.blade.php
+++ resources/views/admin/students/create.blade.php
@@ -3,9 +3,16 @@
         <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet"/>
     @endsection
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            O'quvchi qo'shish
-        </h2>
+        <div class="flex justify-between align-middle">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                O'quvchi qo'shish
+            </h2>
+            <a
+                class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 dark:focus:bg-blue-700"
+                href="{{route('students.index')}}">
+                <i class="fas fa-arrow-left"></i> Orqaga
+            </a>
+        </div>
     </x-slot>
     <div class="py-12">
         <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg mx-auto">
