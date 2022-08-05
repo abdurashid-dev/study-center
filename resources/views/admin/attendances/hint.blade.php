518f09b1316620d624f66d29f9636cb2e759c346
Abdurashid <abdurashiddev@gmail.com>
2022-08-05 16:57:00 +0500

    attendance hint responsive bug fixed

--- resources/views/admin/attendances/hint.blade.php
+++ resources/views/admin/attendances/hint.blade.php
@@ -1,23 +1,23 @@
-<div class="my-3 flex">
-    <div class="flex mx-3">
+<div class="my-3 flex flex-wrap">
+    <div class="flex mx-3 py-1">
         <div
             class="flex items-center justify-center h-6 w-6 rounded-full bg-red-500 dark:bg-red-800 mx-2 text-white">
             <i class="fas fa-times"></i>
         </div>
         Kelmagan
     </div>
-    <div class="flex mx-3">
+    <div class="flex mx-3 py-1">
         <div
             class="flex items-center justify-center h-6 w-6 rounded-full bg-green-500 dark:bg-green-800 mx-2 text-white">
             <i class="fas fa-check"></i>
         </div>
         Kelgan
     </div>
-    <div class="flex mx-3">
+    <div class="flex mx-3 py-1">
         <div
             class="flex items-center justify-center h-6 w-6 rounded-full bg-yellow-500 dark:bg-yellow-800 mx-2 text-white">
             <i class="fas fa-question"></i>
         </div>
-        Sababli kela olmagan
+        Sababli kelmagan
     </div>
 </div>
