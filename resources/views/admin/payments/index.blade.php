bf2ea3e2c7e224e6a3ad45edf8d200495574d657
Abdurashid <abdurashiddev@gmail.com>
2022-08-07 14:21:56 +0500

    payment table added

--- resources/views/admin/payments/index.blade.php
+++ resources/views/admin/payments/index.blade.php
@@ -19,7 +19,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-4">
-                <h1>Payment table</h1>
+                <livewire:student-payment-table/>
             </div>
         </div>
     </div>
