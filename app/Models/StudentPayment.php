4ef9746091b369aeba44c6ccab035712994ca0d7
Abdurashid <abdurashiddev@gmail.com>
2022-08-07 12:11:14 +0500

    payment model and -m added

---
+++ app/Models/StudentPayment.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class StudentPayment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['student_id', 'payment', 'comment'];
+}
---
+++ database/migrations/2022_08_07_070916_create_student_payments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student_payments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('student_id')->references('id')->on('students')->onDelete('cascade');
+            $table->integer('payment');
+            $table->string('comment')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student_payments');
+    }
+};
