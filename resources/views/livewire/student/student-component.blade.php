91fe182256fd43d569ee9c737c71611a2e66a1c3
Abdurashid <abdurashiddev@gmail.com>
2022-08-14 11:20:34 +0500

    search component fixed to tailwind

--- resources/views/livewire/student/student-component.blade.php
+++ resources/views/livewire/student/student-component.blade.php
@@ -19,6 +19,10 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-4">
+                <x-search>
+                    <x-search-option value="name" title="Ism"/>
+                    <x-search-option value="address" title="Manzil"/>
+                </x-search>
             </div>
         </div>
     </div>
