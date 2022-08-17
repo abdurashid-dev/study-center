621075dde9df1245c54d9b36ed397e66e5cca1a2
Abdurashid <abdurashiddev@gmail.com>
2022-08-17 17:35:07 +0500

    payment index table done

--- app/Http/Livewire/Payment/StudentPayment.php
+++ app/Http/Livewire/Payment/StudentPayment.php
@@ -4,12 +4,31 @@ namespace App\Http\Livewire\Payment;
 
 use App\Models\StudentPayment as StudentPaymentModel;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class StudentPayment extends Component
 {
+    use WithPagination;
+
+    public $search = '';
+
     public function render()
     {
-        $payments = StudentPaymentModel::with('student')->orderByDesc('created_at')->get();
+        $payments = StudentPaymentModel::query()
+            ->join('students', 'students.id', '=', 'student_payments.student_id')
+            //if search is empty, return all payments
+            ->when(!$this->search, function ($query) {
+                return $query;
+            })
+            //if search is not empty, return payments with student that match search
+            ->when($this->search, function ($query) {
+                return $query->whereHas('student', function ($query) {
+                    return $query->where('full_name', 'like', '%' . $this->search . '%');
+                });
+            })
+            ->orderBy('created_at', 'desc')
+            ->select('student_payments.*', 'students.full_name as student_full_name', 'students.slug as student_slug')
+            ->paginate(20);
         return view('livewire.payment.student-payment', compact('payments'));
     }
 }
--- resources/views/livewire/payment/student-payment.blade.php
+++ resources/views/livewire/payment/student-payment.blade.php
@@ -5,13 +5,13 @@
     <x-slot name="header">
         <div class="flex justify-between align-middle">
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                O'quvchilar
+                To'lovlar
             </h2>
             <a
                 class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 dark:focus:bg-blue-700"
-                href="{{route('students.create')}}">
+                href="{{route('payment.create')}}">
                 <i class="fas fa-plus"></i>
-                Yangi O'quvchi
+                To'lov qo'shish
             </a>
         </div>
     </x-slot>
@@ -37,71 +37,39 @@
                                 Vaqt
                             </th>
                             <th scope="col" class="py-3 px-6">
-                                Guruh
-                            </th>
-                            <th scope="col" class="py-3 px-6">
                                 Harakatlar
                             </th>
                         </tr>
                         </thead>
                         <tbody>
-                        @forelse($students as $student)
+                        @forelse($payments as $payment)
                             <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                                 <td class="py-3 px-6">
-                                    {{(($students->currentpage()-1)*$students->perpage()+($loop->index+1)) }}
+                                    {{(($payments->currentpage()-1)*$payments->perpage()+($loop->index+1)) }}
                                 </td>
                                 <td class="py-3 px-6">
-                                    <a href="{{route('students.show', $student->slug)}}"
+                                    <a href="{{route('students.show', $payment->student->slug)}}"
                                        class="text-gray-900 dark:text-white">
-                                        {{$student->full_name}}
+                                        {{$payment->student->full_name}}
                                     </a>
                                 </td>
                                 <td class="py-3 px-6">
-                                    <a href="{{route('students.show', $student->slug)}}"
-                                       class="text-gray-900 dark:text-white">
-                                        {{$student->address}}
-                                    </a>
-                                </td>
-                                <td class="py-3 px-6">
-                                    @foreach($student->phones as $phone)
-                                        <a href="tel:{{$phone->phone_number}}" class="whitespace-nowrap">
-                                            {{$phone->phone_number}}
-                                        </a><br>
-                                    @endforeach
-                                </td>
-                                <td class="py-3 px-6">
-                                    <a href="{{route('students.show', $student->slug)}}"
+                                    <a href="{{route('students.show', $payment->student->slug)}}"
                                        class="text-gray-900 dark:text-white whitespace-nowrap">
-                                        @forelse($student->groups as $group)
-                                            {{$group->group->name}}<br>
-                                        @empty
-                                            <span
-                                                class="bg-red-100 text-red-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-red-200 dark:text-red-900 whitespace-nowrap">
-                                                Guruh topilmadi :(
-                                            </span>
-                                        @endforelse
+                                        {{number_format($payment->payment, 0, ',', ' ')}} uzs
                                     </a>
                                 </td>
                                 <td class="py-3 px-6">
-                                    <div class="inline-flex rounded-md shadow-sm">
-                                        <a href="{{route('students.show', $student->slug)}}" aria-current="page"
-                                           class="py-2 px-4 text-sm font-medium text-grey-700 bg-wblue rounded-l-lg border border-gray-200 hover:bg-blue-100 hover:text-grey-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-grey-700 dark:bg-blue-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-500 dark:focus:text-white whitespace-nowrap">
-                                            <i class="fas fa-eye"></i>
-                                            Ko'rish
-                                        </a>
-                                        <a href="{{route('students.edit', $student->slug)}}"
-                                           class="py-2 px-4 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-grey-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-grey-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white whitespace-nowrap">
-                                            <i class="fas fa-edit"></i>
-                                            Tahrirlash
-                                        </a>
-                                        <form action="{{route('students.destroy', $student)}}">
-                                            <button type="submit"
-                                                    class="delete-btn py-2 px-4 text-sm font-medium text-gray-900 bg-white rounded-r-md border border-gray-200 hover:bg-gray-100 hover:text-grey-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-grey-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white whitespace-nowrap">
-                                                <i class="fas fa-trash-alt"></i>
-                                                O'chirish
-                                            </button>
-                                        </form>
-                                    </div>
+                                    <a href="{{route('students.show', $payment->student->slug)}}"
+                                       class="text-gray-900 dark:text-white whitespace-nowrap">
+                                        {{$payment->created_at->format('d-F-Y')}}
+                                    </a>
+                                </td>
+                                <td class="py-4 px-8">
+                                    <a href="#"
+                                       class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 whitespace-nowrap">
+                                        <i class="fas fa-eye"></i> Ko'rish
+                                    </a>
                                 </td>
                             </tr>
                         @empty
@@ -113,13 +81,12 @@
                     </table>
                 </div>
                 <div class="my-3">
-                    {{$students->links()}}
+                    {{$payments->links()}}
                 </div>
             </div>
         </div>
     </div>
     @section('scripts')
         @include('links.toastr-js')
-        @include('links.sweetalert')
     @endsection
 </div>
