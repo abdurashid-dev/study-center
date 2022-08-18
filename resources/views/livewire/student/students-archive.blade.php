a9c32a59b3184be4be4dc3fd45db7077a6946822
Abdurashid <abdurashiddev@gmail.com>
2022-08-18 08:28:24 +0500

    archive done

--- resources/views/livewire/student/students-archive.blade.php
+++ resources/views/livewire/student/students-archive.blade.php
@@ -3,16 +3,14 @@
         @include('links.toastr-css')
     @endsection
     <x-slot name="header">
-        <div class="flex justify-between align-middle">
+        <div class="flex justify-between items-center">
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                 Arxiv
             </h2>
-            {{--            <a--}}
-            {{--                class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 dark:focus:bg-blue-700"--}}
-            {{--                href="{{route('students.create')}}">--}}
-            {{--                <i class="fas fa-plus"></i>--}}
-            {{--                Yangi O'quvchi--}}
-            {{--            </a>--}}
+            <a href="#" title="O'chirilgan o'quvchilar soni"
+               class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                {{ $students->count() }}
+            </a>
         </div>
     </x-slot>
 
