19607108b9270d0d7e23ffc9c9181d6d2d31af62
Abdurashid <abdurashiddev@gmail.com>
2022-08-22 13:06:17 +0500

    dashboard changes and 404page added

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,7 +7,7 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            @include('partials.dashborad-cards')
+            @include('partials.dashboard-cards')
 
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-4 mt-5">
                 <livewire:livewire-line-chart
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.welcome-layout')
+@section('content')
+    <div class="text-center my-auto">
+        <h1 class="display-2 text-secondary">404</h1>
+        <h1>Sahifa topilmadi</h1>
+    </div>
+@endsection
