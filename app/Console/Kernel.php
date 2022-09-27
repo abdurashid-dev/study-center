83100bfeec565009408894889116a7692b627b46
Abdurashid-dev <abdurashiddev@gmail.com>
2022-09-27 14:31:16 +0500

    some changes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -18,6 +18,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('payment:monthly')
             ->appendOutputTo('schedule.log')
             ->timezone('Asia/Tashkent')
+//            ->everyMinute();
             ->monthlyOn(1, '05:00');
     }
 
