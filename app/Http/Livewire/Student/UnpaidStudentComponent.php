4fdccccb63b3d4f35365545ba8323b73ac0ed9ec
Abdurashid-dev <abdurashiddev@gmail.com>
2022-10-05 16:35:13 +0500

    dashboard unpaid students bug fixed

--- app/Http/Livewire/Student/UnpaidStudentComponent.php
+++ app/Http/Livewire/Student/UnpaidStudentComponent.php
@@ -10,10 +10,10 @@ class UnpaidStudentComponent extends Component
 {
     public function render()
     {
-        $students = Student::join('student_groups', 'student_groups.student_id', 'students.id')
-            ->join('student_phone_numbers', function ($join) {
-                $join->on('students.id', '=', \Illuminate\Support\Facades\DB::raw('student_phone_numbers.student_id AND student_phone_numbers.id = (SELECT MAX(id) FROM student_phone_numbers WHERE student_id = students.id)'));
-            })
+        $students = Student::
+        join('student_phone_numbers', function ($join) {
+            $join->on('students.id', '=', \Illuminate\Support\Facades\DB::raw('student_phone_numbers.student_id AND student_phone_numbers.id = (SELECT MAX(id) FROM student_phone_numbers WHERE student_id = students.id)'));
+        })
             ->join('student_balances', function ($query) {
                 $query->on('student_balances.student_id', 'students.id')
                     ->where('student_balances.balance', '<', 0);
