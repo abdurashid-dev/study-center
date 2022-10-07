f7c762fb7d04e8219e457da184ef9c8f18c6fee4
Abdurashid-dev <abdurashiddev@gmail.com>
2022-10-07 10:51:56 +0500

    time of lessons section started

--- resources/views/admin/groups/index.blade.php
+++ resources/views/admin/groups/index.blade.php
@@ -7,12 +7,20 @@
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                 Guruhlar
             </h2>
-            <a
-                class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 dark:focus:bg-blue-700"
-                href="{{route('groups.create')}}">
-                <i class="fas fa-plus"></i>
-                Yangi Guruh
-            </a>
+            <div class="flex justify-between">
+                <a
+                    class="block mr-1 text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 dark:focus:bg-green-700"
+                    href="{{route('groups.create')}}">
+                    <i class="far fa-calendar-alt"></i>
+                    Taqvim
+                </a>
+                <a
+                    class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 dark:focus:bg-blue-700"
+                    href="{{route('groups.create')}}">
+                    <i class="fas fa-plus"></i>
+                    Yangi Guruh
+                </a>
+            </div>
         </div>
     </x-slot>
 
