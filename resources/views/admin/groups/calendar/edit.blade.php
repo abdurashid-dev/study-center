04816a7ed78ca4b029efd24e802db13b11a10156
Abdurashid-dev <abdurashiddev@gmail.com>
2022-10-08 10:08:57 +0500

    calendar edit in process

--- resources/views/admin/groups/calendar/edit.blade.php
+++ resources/views/admin/groups/calendar/edit.blade.php
@@ -19,8 +19,55 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-4">
-                
+                <div class="flex justify-between">
+                    <div style="width: 47%">
+                        <x-jet-label for="group" value="Hafta kunlari"/>
+                        <select style="margin: 4px 0 8px; padding: 8px" class="form-select appearance-none
+                      block
+                      w-full
+                      text-base
+                      font-normal
+                      text-gray-700
+                      bg-white bg-clip-padding bg-no-repeat
+                      border border-solid border-gray-300
+                      rounded
+                      transition
+                      ease-in-out
+                      focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                                aria-label="Default select example">
+                            <option selected>Hafta kunlari</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </select>
+                        <x-jet-input-error for="address" class="mt-2"/>
+                    </div>
+                    <div style="width: 47%">
+                        <x-jet-label for="address" value="Vaqt"/>
+                        <div class="flex justify-center">
+                            <div class="timepicker relative form-floating mb-3 xl:w-96">
+                                <input type="text"
+                                       class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                                       placeholder="Select a date"/>
+                                <label for="floatingInput" class="text-gray-700">Select a time</label>
+                                <button tabindex="0" type="button" class="timepicker-toggle-button"
+                                        data-mdb-toggle="timepicker">
+                                    <i class="fas fa-clock timepicker-icon"></i>
+                                </button>
+                            </div>
+                        </div>
+                        <x-jet-input-error for="address" class="mt-2"/>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
+    @section('scripts')
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"/>
+        <link rel="stylesheet"
+              href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"/>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tw-elements/dist/css/index.min.css"/>
+        <script src="https://cdn.tailwindcss.com"></script>
+        <script src="https://cdn.jsdelivr.net/npm/tw-elements/dist/js/index.min.js"></script>
+    @endsection
 </x-app-layout>
