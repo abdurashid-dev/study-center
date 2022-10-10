1b39c82434e899f89d2e305262983ca45ea6cb44
Abdurashid-dev <abdurashiddev@gmail.com>
2022-10-10 09:11:54 +0500

    group time index table bug fixed

--- resources/views/admin/groups/calendar/index.blade.php
+++ resources/views/admin/groups/calendar/index.blade.php
@@ -40,26 +40,28 @@
                         </thead>
                         <tbody>
                         @forelse($groups as $group)
-                            <td class="py-3 px-6">
-                                {{(($groups->currentpage()-1)*$groups->perpage()+($loop->index+1)) }}
-                            </td>
-                            <td class="py-3 px-6">
-                                {{$group->name}}
-                            </td>
-                            <td class="py-6 px-6">
-                                <a
-                                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 dark:focus:bg-blue-700"
-                                    href="{{route('groups-times.show', $group->slug)}}">
-                                    Ko'rish
-                                </a>
-                            </td>
-                            <td class="py-3 px-6">
-                                <a
-                                    class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 dark:focus:bg-green-700"
-                                    href="{{route('groups-times.edit', $group->slug)}}">
-                                    Tahrirlash
-                                </a>
-                            </td>
+                            <tr>
+                                <td class="py-3 px-6">
+                                    {{(($groups->currentpage()-1)*$groups->perpage()+($loop->index+1)) }}
+                                </td>
+                                <td class="py-3 px-6">
+                                    {{$group->name}}
+                                </td>
+                                <td class="py-6 px-6">
+                                    <a
+                                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 dark:focus:bg-blue-700"
+                                        href="{{route('groups-times.show', $group->slug)}}">
+                                        Ko'rish
+                                    </a>
+                                </td>
+                                <td class="py-3 px-6">
+                                    <a
+                                        class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 dark:focus:bg-green-700"
+                                        href="{{route('groups-times.edit', $group->slug)}}">
+                                        Tahrirlash
+                                    </a>
+                                </td>
+                            </tr>
                         @empty
                         @endforelse
                         </tbody>
