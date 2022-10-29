ea9b16d953e667a919d4ee595eafaedb05e92970
Abdurashid-dev <abdurashiddev@gmail.com>
2022-10-29 12:18:39 +0500

    dtm index table added

---
+++ app/Http/Livewire/DtmTable.php
@@ -0,0 +1,217 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Dtm;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Carbon;
+use PowerComponents\LivewirePowerGrid\{Button,
+    Column,
+    Exportable,
+    Footer,
+    Header,
+    PowerGrid,
+    PowerGridComponent,
+    PowerGridEloquent
+};
+use PowerComponents\LivewirePowerGrid\Rules\{RuleActions};
+use PowerComponents\LivewirePowerGrid\Traits\ActionButton;
+
+final class DtmTable extends PowerGridComponent
+{
+    use ActionButton;
+
+    /*
+    |--------------------------------------------------------------------------
+    |  Features Setup
+    |--------------------------------------------------------------------------
+    | Setup Table's general features
+    |
+    */
+    public function setUp(): array
+    {
+//        $this->showCheckBox();
+        return [
+            Exportable::make('export')
+                ->striped()
+                ->type(Exportable::TYPE_XLS, Exportable::TYPE_CSV),
+            Header::make()->showSearchInput(),
+            Footer::make()
+                ->showPerPage()
+                ->showRecordCount(),
+        ];
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    |  Datasource
+    |--------------------------------------------------------------------------
+    | Provides data to your Table using a Model or Collection
+    |
+    */
+
+    /**
+     * PowerGrid datasource.
+     *
+     * @return Builder<\App\Models\Dtm>
+     */
+    public function datasource(): Builder
+    {
+        return Dtm::query();
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    |  Relationship Search
+    |--------------------------------------------------------------------------
+    | Configure here relationships to be used by the Search and Table Filters.
+    |
+    */
+
+    /**
+     * Relationship search.
+     *
+     * @return array<string, array<int, string>>
+     */
+    public function relationSearch(): array
+    {
+        return [];
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    |  Add Column
+    |--------------------------------------------------------------------------
+    | Make Datasource fields available to be used as columns.
+    | You can pass a closure to transform/modify the data.
+    |
+    */
+    public function addColumns(): PowerGridEloquent
+    {
+        return PowerGrid::eloquent()
+            ->addColumn('id')
+            ->addColumn('name')
+            ->addColumn('description', fn(Dtm $dtm) => \Str::limit($dtm->description, 20))
+            ->addColumn('count_tests')
+            ->addColumn('created_at_formatted', fn(Dtm $model) => Carbon::parse($model->created_at)->format('d/m/Y H:i:s'));
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    |  Include Columns
+    |--------------------------------------------------------------------------
+    | Include the columns added columns, making them visible on the Table.
+    | Each column can be configured with properties, filters, actions...
+    |
+    */
+
+    /**
+     * PowerGrid Columns.
+     *
+     * @return array<int, Column>
+     */
+    public function columns(): array
+    {
+        return [
+            Column::make('ID', 'id')
+                ->sortable()
+                ->makeInputRange(),
+
+            Column::make('DTM NOMI', 'name')
+                ->sortable()
+                ->searchable()
+                ->editOnClick()
+                ->makeInputText(),
+
+            Column::make('IZOH', 'description')
+                ->sortable()
+                ->searchable(),
+
+            Column::make('TESTLAR SONI', 'count_tests')
+                ->sortable()
+                ->makeInputRange(),
+
+            Column::make('TEST OLINGAN SANA', 'created_at_formatted', 'created_at')
+                ->searchable()
+                ->sortable()
+                ->makeInputDatePicker(),
+        ];
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Actions Method
+    |--------------------------------------------------------------------------
+    | Enable the method below only if the Routes below are defined in your app.
+    |
+    */
+
+    /**
+     * PowerGrid Dtm Action Buttons.
+     *
+     * @return array<int, Button>
+     */
+
+
+    public function actions(): array
+    {
+        return [
+            Button::make('show', 'SHOW')
+                ->class('bg-blue-500 cursor-pointer text-white px-3 py-2.5 rounded text-sm')
+                ->target(false)
+                ->route('dtm.show', ['dtm' => 'id']),
+        ];
+    }
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Actions Rules
+    |--------------------------------------------------------------------------
+    | Enable the method below to configure Rules for your Table and Action Buttons.
+    |
+    */
+
+    /**
+     * PowerGrid Dtm Action Rules.
+     *
+     * @return array<int, RuleActions>
+     */
+
+    /*
+    public function actionRules(): array
+    {
+       return [
+
+           //Hide button edit for ID 1
+            Rule::button('edit')
+                ->when(fn($dtm) => $dtm->id === 1)
+                ->hide(),
+        ];
+    }
+    */
+    /*-------------------------------------------*/
+    /*|Toggle update|*/
+    public function onUpdatedToggleable($id, $field, $value): void
+    {
+        Dtm::query()->find($id)->update([
+            $field => $value,
+        ]);
+    }
+    /*-------------------------------------------*/
+    /*|onUpdate|*/
+    public bool $showErrorBag = true;
+    public $name = null;
+
+    protected array $rules = [
+        'name.*' => ['required', 'min:2', 'max:255'],
+    ];
+
+    public function onUpdatedEditable($id, $field, $value): void
+    {
+        $this->validate();
+        Dtm::query()->find($id)->update([
+            $field => $value,
+        ]);
+    }
+}
--- resources/views/admin/dtms/index.blade.php
+++ resources/views/admin/dtms/index.blade.php
@@ -19,70 +19,71 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-4">
-                <x-search/>
-                <div class="overflow-x-auto relative shadow-md sm:rounded-lg mt-3">
-                    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                        <tr>
-                            <th scope="col" class="py-3 px-6">
-                                #
-                            </th>
-                            <th scope="col" class="py-3 px-6">
-                                DTM nomi
-                            </th>
-                            <th scope="col" class="py-3 px-6">
-                                Guruh
-                            </th>
-                            <th scope="col" class="py-3 px-6">
-                                Sana
-                            </th>
-                            <th scope="col" class="py-3 px-6">
-                                Harakatlar
-                            </th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @forelse($dtms as $dtm)
-                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                <td class="py-3 px-6">
-                                    {{(($dtms->currentpage()-1)*$dtms->perpage()+($loop->index+1)) }}
-                                </td>
-                                <td class="py-3 px-6">
-                                    <a href="{{route('dtm.show', $dtm->slug)}}"
-                                       class="text-gray-900 dark:text-white">
-                                        {{$dtm->name}}
-                                    </a>
-                                </td>
-                                <td class="py-3 px-6">
-                                    <a href="{{route('dtm.show', $dtm->slug)}}"
-                                       class="text-gray-900 dark:text-white whitespace-nowrap">
-                                        {{$dtm->getGroupName($dtm->group_id)}}
-                                    </a>
-                                </td>
-                                <td class="py-3 px-6">
-                                    <a href="{{route('dtm.show', $dtm->slug)}}"
-                                       class="text-gray-900 dark:text-white whitespace-nowrap">
-                                        {{$dtm->created_at->format('d-F-Y')}}
-                                    </a>
-                                </td>
-                                <td class="py-4 px-8">
-                                    <a href="{{route('dtm.show', $dtm->slug)}}"
-                                       class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 whitespace-nowrap">
-                                        <i class="fas fa-eye"></i> Ko'rish
-                                    </a>
-                                </td>
-                            </tr>
-                        @empty
-                            <td colspan="6" class="text-center py-3 px-6">
-                                DTM natijalari topilmadi :(
-                            </td>
-                        @endforelse
-                        </tbody>
-                    </table>
-                </div>
-                <div class="my-3">
-                    {{$dtms->links()}}
-                </div>
+                <livewire:dtm-table/>
+                {{--                <x-search/>--}}
+                {{--                <div class="overflow-x-auto relative shadow-md sm:rounded-lg mt-3">--}}
+                {{--                    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">--}}
+                {{--                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">--}}
+                {{--                        <tr>--}}
+                {{--                            <th scope="col" class="py-3 px-6">--}}
+                {{--                                #--}}
+                {{--                            </th>--}}
+                {{--                            <th scope="col" class="py-3 px-6">--}}
+                {{--                                DTM nomi--}}
+                {{--                            </th>--}}
+                {{--                            <th scope="col" class="py-3 px-6">--}}
+                {{--                                Guruh--}}
+                {{--                            </th>--}}
+                {{--                            <th scope="col" class="py-3 px-6">--}}
+                {{--                                Sana--}}
+                {{--                            </th>--}}
+                {{--                            <th scope="col" class="py-3 px-6">--}}
+                {{--                                Harakatlar--}}
+                {{--                            </th>--}}
+                {{--                        </tr>--}}
+                {{--                        </thead>--}}
+                {{--                        <tbody>--}}
+                {{--                        @forelse($dtms as $dtm)--}}
+                {{--                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">--}}
+                {{--                                <td class="py-3 px-6">--}}
+                {{--                                    {{(($dtms->currentpage()-1)*$dtms->perpage()+($loop->index+1)) }}--}}
+                {{--                                </td>--}}
+                {{--                                <td class="py-3 px-6">--}}
+                {{--                                    <a href="{{route('dtm.show', $dtm->slug)}}"--}}
+                {{--                                       class="text-gray-900 dark:text-white">--}}
+                {{--                                        {{$dtm->name}}--}}
+                {{--                                    </a>--}}
+                {{--                                </td>--}}
+                {{--                                <td class="py-3 px-6">--}}
+                {{--                                    <a href="{{route('dtm.show', $dtm->slug)}}"--}}
+                {{--                                       class="text-gray-900 dark:text-white whitespace-nowrap">--}}
+                {{--                                        {{$dtm->getGroupName($dtm->group_id)}}--}}
+                {{--                                    </a>--}}
+                {{--                                </td>--}}
+                {{--                                <td class="py-3 px-6">--}}
+                {{--                                    <a href="{{route('dtm.show', $dtm->slug)}}"--}}
+                {{--                                       class="text-gray-900 dark:text-white whitespace-nowrap">--}}
+                {{--                                        {{$dtm->created_at->format('d-F-Y')}}--}}
+                {{--                                    </a>--}}
+                {{--                                </td>--}}
+                {{--                                <td class="py-4 px-8">--}}
+                {{--                                    <a href="{{route('dtm.show', $dtm->slug)}}"--}}
+                {{--                                       class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 whitespace-nowrap">--}}
+                {{--                                        <i class="fas fa-eye"></i> Ko'rish--}}
+                {{--                                    </a>--}}
+                {{--                                </td>--}}
+                {{--                            </tr>--}}
+                {{--                        @empty--}}
+                {{--                            <td colspan="6" class="text-center py-3 px-6">--}}
+                {{--                                DTM natijalari topilmadi :(--}}
+                {{--                            </td>--}}
+                {{--                        @endforelse--}}
+                {{--                        </tbody>--}}
+                {{--                    </table>--}}
+                {{--                </div>--}}
+                {{--                <div class="my-3">--}}
+                {{--                    {{$dtms->links()}}--}}
+                {{--                </div>--}}
             </div>
         </div>
     </div>
