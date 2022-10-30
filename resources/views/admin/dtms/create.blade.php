ac29efac7919e4de5f026d186ab0da773b6211cd
Abdurashid-dev <abdurashiddev@gmail.com>
2022-10-30 13:41:18 +0500

    dtm form updated

--- resources/views/admin/dtms/create.blade.php
+++ resources/views/admin/dtms/create.blade.php
@@ -8,8 +8,8 @@
                 DTM qo'shish
             </h2>
             <a
-                class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 dark:focus:bg-blue-700"
-                href="{{route('dtm.index')}}">
+                    class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 dark:focus:bg-blue-700"
+                    href="{{route('dtm.index')}}">
                 <i class="fas fa-arrow-left"></i> Orqaga
             </a>
         </div>
@@ -27,7 +27,7 @@
             @endif
             <form action="{{route('dtm.store')}}" method="POST">
                 @csrf
-                @include('admin.dtms.create-form')
+                @include('admin.dtms.form')
             </form>
         </div>
     </div>
